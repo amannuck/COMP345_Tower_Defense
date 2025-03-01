930ee5e9bef2697826132fcb53c3510b690beaea
ManilongMangbu <150404542+ManilongMangbu@users.noreply.github.com>
2025-03-02 01:22:53 +0800

    A2 tower.h

--- tower.h
+++ tower.h
@@ -22,8 +22,6 @@ using namespace std;
  * - Attack power, range, fire rate
  * - Purchase & refund value
  * - Upgrade mechanics
- *
- * Towers can now be placed and removed from the map.
  */
 class Tower {
 protected:
@@ -37,71 +35,18 @@ protected:
     int upgradeCost; ///< Gold required to upgrade
 
 public:
-    /**
-     * @brief Constructs a Tower object.
-     * @param x X-coordinate of the tower.
-     * @param y Y-coordinate of the tower.
-     * @param cost Initial cost of the tower.
-     * @param refund Refund value when selling the tower.
-     * @param range Attack range of the tower.
-     * @param power Attack power of the tower.
-     * @param fireRate Rate of fire of the tower.
-     * @param upgradeCost Cost to upgrade the tower.
-     */
     Tower(int x, int y, int cost, int refund, int range, int power, int fireRate, int upgradeCost);
     virtual ~Tower() {}
 
-    /**
-     * @brief Attacks critters within range.
-     * @param critters List of critters on the map.
-     */
     virtual void attack(vector<Critter>& critters) = 0;
-
-    /**
-     * @brief Upgrades the tower (increases damage and refund value).
-     */
     void upgrade();
 
-    /**
-     * @brief Gets the X-coordinate of the tower.
-     * @return X-coordinate.
-     */
     int getX() { return x; }
-
-    /**
-     * @brief Gets the Y-coordinate of the tower.
-     * @return Y-coordinate.
-     */
     int getY() { return y; }
-
-    /**
-     * @brief Gets the attack range of the tower.
-     * @return Attack range.
-     */
     int getRange() { return range; }
-
-    /**
-     * @brief Gets the attack power of the tower.
-     * @return Attack power.
-     */
     int getPower() { return power; }
-
-    /**
-     * @brief Gets the purchase cost of the tower.
-     * @return Purchase cost.
-     */
     int getBuyCost() { return buyCost; }
-
-    /**
-     * @brief Gets the refund value of the tower.
-     * @return Refund value.
-     */
     int getRefundValue() { return refundValue; }
-
-    /**
-     * @brief Gets the current level of the tower.
-     * @return Tower level.
-     */
     int getLevel() { return level; }
 };
 
@@ -111,17 +56,7 @@ public:
  */
 class BasicTower : public Tower {
 public:
-    /**
-     * @brief Constructs a BasicTower object.
-     * @param x X-coordinate of the tower.
-     * @param y Y-coordinate of the tower.
-     */
     BasicTower(int x, int y);
-
-    /**
-     * @brief Attacks a single critter within range.
-     * @param critters List of critters on the map.
-     */
     void attack(vector<Critter>& critters) override;
 };
 
@@ -131,19 +66,15 @@ public:
  */
 class AoETower : public Tower {
 public:
-    /**
-     * @brief Constructs an AoETower object.
-     * @param x X-coordinate of the tower.
-     * @param y Y-coordinate of the tower.
-     */
     AoETower(int x, int y);
-
-    /**
-     * @brief Attacks multiple critters within range.
-     * @param critters List of critters on the map.
-     */
     void attack(vector<Critter>& critters) override;
 };
 
+/**
+ * @brief Allows the user to place a tower interactively.
+ * @param map Reference to the game map.
+ * @param towers Vector of currently placed towers.
+ */
+void placeTowerInteractive(Map& map, vector<Tower*>& towers);
 
 #endif // TOWER_H
